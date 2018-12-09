40e75f65fbcc2720a22447a0dafb70136d660f71
vagrant <vagrant@localhost.localdomain>
2018-12-09 02:03:17 +0000

    group作成

---
+++ db/migrate/20181209014219_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :groups do |t|
+      t.string :name, null: false
+      t.boolean :public, default: true, null: false
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
